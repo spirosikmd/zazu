4153a28305a81e1d0e5008757c745b1c2559881c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-02-27 22:51:10 +0100

    Added source code pro font and applied base styling, basic support for updating, and hotkeys initial setup

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 (function (angular) {
-  angular.module('app', [])
+  angular.module('app', ['cfp.hotkeys'])
     .service('TodoService', function () {
       var todos = [{
+        id: 1,
         checked: true,
         label: 'Todo item one'
       }, {
+        id: 2,
         checked: false,
         label: 'Todo item two'
       }];
@@ -14,37 +16,96 @@
       }
 
       function create (todo) {
+        todo.id = todos.length + 1;
         todos.push(todo);
+      }
+
+      function update (id, key, value) {
+        for (var index = 0; index < todos.length; index++) {
+          var current = todos[index];
+          if (current.id === id) {
+            current[key] = value;
+            break;
+          }
+        }
       }
 
       return {
         get: get,
-        create: create
+        create: create,
+        update: update
       };
     })
-    .controller('TodoController', function (TodoService) {
-      const defaultTodo = {
+    .controller('TodoController', function ($scope, TodoService, hotkeys) {
+      var context = this;
+      var defaultTodo = {
         label: '',
         checked: false
       };
-      this.todos = [];
-      this.todo = null;
-
-      this.refresh = function () {
-        this.todos = TodoService.get();
+      context.todos = [];
+      context.todo = null;
+      context.modes = {
+        create: false
       };
 
-      this.reset = function () {
-        this.todo = angular.copy(defaultTodo);
+      context.setMode = function setMode (mode, state) {
+        context.modes[mode] = state;
       };
 
-      this.create = function (todo) {
+      context.refresh = function () {
+        context.todos = TodoService.get();
+      };
+
+      context.reset = function () {
+        context.todo = angular.copy(defaultTodo);
+      };
+
+      context.create = function (todo) {
+        if (todo.label.trim().length === 0) {
+          return;
+        }
         TodoService.create(angular.copy(todo));
-        this.refresh();
-        this.reset();
+        context.refresh();
+        context.reset();
       };
 
-      this.refresh();
-      this.reset();
+      context.updateChecked = function check (id, checked) {
+        TodoService.update(id, 'checked', checked);
+      };
+
+      context.refresh();
+      context.reset();
+
+      hotkeys.bindTo($scope)
+        .add({
+          combo: 'mod+n',
+          description: 'Create new todo',
+          callback: function (event) {
+            event.preventDefault();
+            context.setMode('create', true);
+          }
+        })
+        .add({
+          combo: 'esc',
+          description: 'Default state',
+          allowIn: ['INPUT'],
+          callback: function (event) {
+            event.preventDefault();
+            context.setMode('create', false);
+          }
+        });
+    })
+    .directive('focus', function ($timeout) {
+      return {
+        link: function (scope, element, attributes) {
+          scope.$watch(attributes.focus, function (value) {
+            if (value === true) {
+              $timeout(function () {
+                element[0].focus();
+              });
+            }
+          });
+        }
+      }
     });
 })(angular);
