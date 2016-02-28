967d2c212c961d1dd168373cbeca5defac8fdb03
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-02-28 01:10:14 +0100

    Basic styling of inputs

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -21,12 +21,29 @@ export class TodoService {
   }
 
   update (id, key, value) {
+    var index = this.find(id);
+    if (index === -1) {
+      return;
+    }
+    var current = this.todos[index];
+    current[key] = value;
+  }
+
+  remove (id) {
+    var index = this.find(id);
+    if (index === -1) {
+      return;
+    }
+    this.todos.splice(index, 1);
+  }
+
+  find (id) {
     for (var index = 0; index < this.todos.length; index++) {
       var current = this.todos[index];
       if (current.id === id) {
-        current[key] = value;
-        break;
+        return index;
       }
     }
+    return -1;
   }
 }
