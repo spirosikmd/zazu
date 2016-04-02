313ece1c88e3cffd50e9e2168f1ab95d120043c7
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-04-02 16:30:32 +0200

    Include only production needed node modules

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-gulp package --env=production && electron-packager . Zazu --platform=darwin --arch=x64 --version=0.37.3 --overwrite
+gulp package --env=production && electron-packager . Zazu --platform=darwin --arch=x64 --version=0.37.3 --overwrite \
+    --ignore="^/node_modules/(?!(electron-localshortcut))"
