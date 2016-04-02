346032d29c717f3c42de72ee054175c1bec04fb9
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-04-02 16:49:33 +0200

    Include hotkeys and source code pro packages

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 gulp package --env=production && electron-packager . Zazu --platform=darwin --arch=x64 --version=0.37.3 --overwrite \
-    --ignore="^/node_modules/(?!(electron-localshortcut))"
+    --ignore="^/node_modules/(?!(electron-localshortcut|angular-hotkeys|source-code-pro))"
