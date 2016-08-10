9174003c7cee34e3ca837b9776fb1d73b29e5fd7
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-08-11 00:40:09 +0200

    Ignored more dev folders and files during packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 gulp package --env=production && electron-packager . Zazu --platform=darwin --arch=x64 --version=1.3.2 --overwrite \
-    --ignore="node_modules|typings|scripts|resources|.idea|src|.babelrc|.gitignore|.travis.yml|gulpfile.js|karma.conf.js|README.md|zazu.config.json|coverage"
+    --ignore="node_modules|scripts|resources|.idea|src|.babelrc|.gitignore|.travis.yml|gulpfile.js|karma.conf.js|README.md|zazu.config.json|coverage|typings|tsconfig.json|typings.json"
